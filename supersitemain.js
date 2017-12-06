db35da0c2316d585c593cf61daaa726700534c7f
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2017-12-06 13:22:48 -0500

    Update supersitemain.js

--- supersitemain.js
+++ supersitemain.js
@@ -1,3 +1,3 @@
-var site=window.opener.location.pathname;
+function phosphorus(){var site=window.opener.location.pathname;
 var project=site.split("/")[2];
-window.opener.location.assign("https://phosphorus.github.io/#"+project);
+window.opener.location.assign("https://phosphorus.github.io/#"+project);}
